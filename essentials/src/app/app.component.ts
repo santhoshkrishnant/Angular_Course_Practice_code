6e4a08663426b0493cb9bb3961cf9966b637675b
9802000728 <santhoshkrishnan2001@gmail.com>
2025-10-09 19:19:30 +0530

    Second Commit the practice code

--- essentials/src/app/app.component.ts
+++ essentials/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.componet';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-user';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((u) => u.id === this.selectedUserId)!;
+  }
+
+
+  onSelectUser(id: string) {
+    // const selectedUser = DUMMY_USERS.find((u) => u.id === id);
+    // this.selectedUser.set(selectedUser);
+    // console.log('Selected user with id' + id);
+    this.selectedUserId = id;
+  }
+}
--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -1,7 +1,16 @@
-import { Component, computed, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  Input,
+  Output,
+  input,
+  signal,
+  EventEmitter,
+  output,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-user';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -10,15 +19,45 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  //// Randomaly select a user from the dummy users:
+
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
   // get imagePath() {
   //   return './assets/users/' + this.selectedUser().avatar;
   //   ;
   // }
 
+  ////Decorator to mark a property as an input property and supply configuration metadata.
+  //// The property is bound to a value passed in from the component's parent.
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  ////Decorator to mark a property as an output property:
+  //// The property is bound to a value passed in from the component's parent
+  @Output() select = new EventEmitter<string>();
+
+  // //Using a getter to construct the image path dynamically:
+  get imagePath() {
+    return '../../assets/users/' + this.avatar;
+  }
+
+  //// Using signal and input to make the component more reactive:
+  // id = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  //// Using signal and output to make the component more reactive:
+  // select = output<string>();
+
+  // //// Using computed to dynamically construct the image path:
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  //// Method to handle user selection:
   onSelectUSer() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
